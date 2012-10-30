515a0154ccdd7296180f2fe509ac1e5c713c9a12
Doug Fritz <dougfritz@gmail.com>
2012-10-30 16:53:52 -0700

    added filtering in and ordering

--- tailbone/tailbone.go
+++ tailbone/tailbone.go
@@ -11,43 +11,47 @@ import (
 	"net/http"
 	"errors"
 	"strings"
+	"strconv"
+	"regexp"
 )
 
-type httpHandler func(w http.ResponseWriter, r *http.Request)
-type requestHandler func(c appengine.Context, r *http.Request) (interface{}, error)
+type HttpHandler func(w http.ResponseWriter, r *http.Request)
+type RequestHandler func(c appengine.Context, r *http.Request) (interface{}, error)
 
-type dict map[string]interface{}
+type Dict map[string]interface{}
 
-func (d dict) Load(c <-chan datastore.Property) error {
+func (d Dict) Load(c <-chan datastore.Property) error {
 	log.Printf("Loading")
 	for p := range c {
-		log.Printf("Property: %s", p)
+    log.Printf("Property: %s, Name: %s, Value: %s", p, p.Name, p.Value)
 		d[p.Name] = p.Value
 	}
 	return nil
 }
 
-func (d dict) saveWithPrefix(prefix string, c chan<- datastore.Property) error {
+func (d Dict) SaveWithPrefix(prefix string, c chan<- datastore.Property) error {
+  return nil
 }
 
-func (d dict) Save(c chan<- datastore.Property) error {
+func (d Dict) Save(c chan<- datastore.Property) error {
 	log.Printf("Saving")
 	defer close(c)
 	for k, v := range d {
 		switch t := v.(type) {
 		case map[string]interface{}:
 			log.Printf("type: map")
-			v.saveWithPrefix(k,c)
+			// v.SaveWithPrefix(k,c)
 		case []interface{}:
 			log.Printf("type: slice")
-		case float64:
-			log.Printf("type: float")
-		case int64:
-			log.Printf("type: int64")
-		case int:
+		case float64, float32:
+			log.Printf("type: float64")
+		case int64, int, int32:
 			log.Printf("type: int")
+      v = v.(float64)
 		case string:
 			log.Printf("type: string")
+		case bool:
+			log.Printf("type: bool")
 		default:
 			log.Printf("type: %s", t)
 		}
@@ -60,7 +64,7 @@ func (d dict) Save(c chan<- datastore.Property) error {
 	return nil
 }
 
-func parseRestfulPath(str string) (kind, id string, err error) {
+func ParseRestfulPath(str string) (kind, id string, err error) {
 	s := strings.Split(str, "/")
 	l := len(s)
 	kind = strings.ToLower(s[2])
@@ -75,11 +79,71 @@ func parseRestfulPath(str string) (kind, id string, err error) {
 	return
 }
 
-func parseBody(r *http.Request) (interface{}, error) {
+var (
+  re_filter = regexp.MustCompile("^([\\w\\-.]+)(!=|==|=|<=|>=|<|>)(.+)$")
+  re_composite_filter = regexp.MustCompile("^(AND|OR)\\((.*)\\)$")
+  re_split = regexp.MustCompile(",\\W*")
+)
+
+func appendFilter(q *datastore.Query, name, op, value string) (*datastore.Query, error) {
+  if op == "==" {
+    op = "="
+  }
+  log.Printf("query filter added %s %s %s", name, op, value)
+  floatValue, err := strconv.ParseFloat(value, 64)
+  if err != nil {
+    return q.Filter(name + " " + op, value), nil
+  }
+  return q.Filter(name + " " + op, floatValue), nil
+}
+
+func parseFilter(q *datastore.Query, filter string) (*datastore.Query, error) {
+  // Go api does not yet support composite filters such as OR or AND so this is left undone
+  composite := re_composite_filter.FindStringSubmatch(filter)
+  if len(composite) == 0 {
+    f := re_filter.FindStringSubmatch(filter)
+    if len(f) != 4 {
+      return nil, errors.New("Incorrectly formated filter.")
+    }
+    var err error
+    q, err = appendFilter(q, f[1], f[2], f[3])
+    if err != nil {
+      return nil, err
+    }
+  } else {
+    return nil, errors.New("Composite filters like OR/AND are not supported in Go version.")
+  }
+  return q, nil
+}
+
+func PopulateQuery(q *datastore.Query, r *http.Request) (*datastore.Query, error) {
+  query := r.URL.Query()
+  if params, ok := query["params"]; ok {
+    log.Printf(params[0])
+  } else {
+    if filters, ok := query["filter"]; ok {
+      var err error
+      for _, filter := range filters {
+        q, err = parseFilter(q, filter)
+        if err != nil {
+          return nil, err
+        }
+      }
+    }
+    if orders, ok := query["order"]; ok {
+      for _, order := range orders {
+        q = q.Order(order)
+      }
+    }
+  }
+  return q, nil
+}
+
+func ParseBody(r *http.Request) (interface{}, error) {
 	contentType := strings.Split(r.Header.Get("Content-Type"),";")[0]
 	switch contentType {
 	case "application/json":
-		var data dict
+		var data Dict
 		decoder := json.NewDecoder(r.Body)
 		err := decoder.Decode(&data)
 		if err != nil {
@@ -93,15 +157,19 @@ func parseBody(r *http.Request) (interface{}, error) {
 }
 
 func Restful(c appengine.Context, r *http.Request) (interface{}, error) {
-	kind, id, err := parseRestfulPath(r.URL.Path)
+	kind, id, err := ParseRestfulPath(r.URL.Path)
 	if (err != nil) { return nil, err }
 	switch r.Method {
 	case "GET":
 		if id == "" {
-			items := []dict{}
+			items := []Dict{}
 			q := datastore.NewQuery(kind)
+      q, err := PopulateQuery(q, r)
+      if err != nil {
+        return nil, err
+      }
 			for t := q.Run(c); ; {
-				var x dict
+        x := make(Dict)
 				key, err := t.Next(&x)
 				c.Infof("Key: %s", key)
 				if err == datastore.Done {
@@ -114,10 +182,10 @@ func Restful(c appengine.Context, r *http.Request) (interface{}, error) {
 			}
 			return items, nil
 		} else {
-			return dict{"method": "get"}, nil
+			return Dict{"method": "get"}, nil
 		}
 	case "POST":
-		data, err := parseBody(r)
+		data, err := ParseBody(r)
 		log.Printf("POST data: %s", data)
 		key := datastore.NewIncompleteKey(c, kind, nil)
 		datastore.Put(c, key, data)
@@ -126,14 +194,14 @@ func Restful(c appengine.Context, r *http.Request) (interface{}, error) {
 		}
 		return data, nil
 	case "PUT":
-		return dict{"method": "put"}, nil
+		return Dict{"method": "put"}, nil
 	case "DELETE":
-		return dict{"method": "delete"}, nil
+		return Dict{"method": "delete"}, nil
 	}
 	return nil, errors.New("Undefined method.")
 }
 
-func Json(h requestHandler) httpHandler {
+func Json(h RequestHandler) HttpHandler {
 	return func(w http.ResponseWriter, r *http.Request) {
 		c := appengine.NewContext(r)
 		resp, err := h(c, r)
@@ -142,7 +210,7 @@ func Json(h requestHandler) httpHandler {
 			//	 case ErrLoginRequired:
 			//		resp = m{"error": err}
 			// }
-			resp = dict{"error": err.Error()}
+      resp = Dict{"type": "error", "message": err.Error()}
 		}
 		if resp != nil {
 			w.Header().Set("content-type", "application/json")
---
+++ tailbone/tailbone.js
@@ -0,0 +1,487 @@
+'use strict';
+
+/*
+ * Bi-Directional data binding with AppEngine and the channel api
+ */
+
+window.tailbone = (function(window, document, undefined) {
+
+var http = {};
+function errorWrapper(fn) {
+  if (fn) {
+    return function(jqXHR, textStatus, errorThrown) {
+      var data;
+      try {
+        data = JSON.parse(jqXHR.responseText);
+      } catch (e) { }
+      fn(data, textStatus, jqXHR, errorThrown);
+    }
+  }
+}
+http.GET = function(url, load, error) {
+  $.ajax({
+    type: 'GET',
+    url: url,
+    success: load,
+    error: errorWrapper(error),
+    dataType: 'json'
+  });
+};
+http.POST = function(url, data, load, error) {
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: JSON.stringify(data),
+    success: load,
+    error: errorWrapper(error),
+    dataType: 'json',
+    contentType: 'application/json'
+  });
+};
+http.DELETE = function(url, load, error) {
+  $.ajax({
+    type: 'DELETE',
+    url: url,
+    success: load,
+    error: errorWrapper(error),
+    dataType: 'json'
+  });
+};
+
+/////////////////////////
+// Events via Channel API
+/////////////////////////
+
+var CONNECTED = false;
+var CONNECTING = false;
+var BACKOFF = 1;
+
+var event_map = {};
+var queue = [];
+var socket;
+var client_id = parseInt(Date.now() / Math.random());
+
+function onOpen() {
+  CONNECTED = true;
+  BACKOFF = 1;
+  for (var i = 0, l = queue.length; i < l; i++) {
+    queue[i].call(this);
+  }
+  queue = [];
+}
+
+function rebind() {
+  var event_bindings = [];
+  for (var name in event_map) {
+    for (var fn in event_map[name]) {
+      event_bindings.push([name, fn]);
+    }
+  }
+  event_map = {};
+  for (var i = 0, l = event_bindings.length; i < l; i++) {
+    ifConnected(bind, event_bindings[i][0], event_bindings[i][1]);
+  }
+}
+
+function onClose() {
+  CONNECTED = false;
+  CONNECTING = false;
+  rebind();
+}
+
+function onError() {
+  // TODO: try reconnecting with backoff or alert system of lack of capability.
+  if (console) {
+    console.warn('Channel not connectable.');
+  }
+}
+
+function onMessage(msg) {
+  var data = JSON.parse(msg.data);
+  if (data.reboot) {
+    // close everything and try and reconnect and rebind the event listeners
+    socket.close();
+    return;
+  }
+  var fns = event_map[data.name];
+  if (fns && fns.length > 0) {
+    for (var i = 0, l = fns.length; i < l; i++) {
+      fns[i].call(fns[i], data.payload);
+    }
+  }
+}
+
+function connect(callback) {
+  if (callback) queue.push(callback);
+  if (!CONNECTING) {
+    http.POST('/api/events/',
+        {'method': 'token', 'client_id': client_id},
+        function(resp) {
+          var channel = new goog.appengine.Channel(resp.token);
+          socket = channel.open();
+          socket.onopen = onOpen;
+          socket.onmessage = onMessage;
+          socket.onclose = onClose;
+          socket.onerror = onError;
+        }
+    );
+  } else {
+    CONNECTING = true;
+  }
+}
+
+function ifConnected(fn, arg1, arg2) {
+  var _this = this;
+  if (CONNECTED) {
+    fn.call(_this, arg1, arg2);
+  } else {
+    connect(function() {fn.call(_this, arg1, arg2);});
+  }
+}
+
+function errorHandler(msg) {
+  if (msg && msg.error && console) {
+    console.warn(msg.error);
+  }
+}
+
+function trigger(name, payload) {
+  http.POST('/api/events/',
+      {'method': 'trigger',
+       'client_id': client_id,
+       'name': name, 'payload': payload},
+       null,
+       errorHandler);
+}
+
+function bind(name, fn) {
+  event_map[name] = event_map[name] || [];
+  event_map[name].push(fn);
+  http.POST('/api/events/',
+      {'method': 'bind',
+       'client_id': client_id,
+       'name': name},
+       null,
+       errorHandler);
+}
+
+function unbind(name, fn) {
+  if (name) {
+    if (fn) {
+      var index = event_map[name].indexOf(fn);
+      if (index < 0) {
+        throw new Error('No matching function exists.');
+      } else {
+        delete event_map[name][index];
+      }
+    } else {
+      delete event_map[name];
+    }
+  } else {
+    event_map = {};
+  }
+  http.POST('/api/events/',
+      {'method': 'unbind',
+       'client_id': client_id, 'name': name},
+      null,
+      errorHandler);
+}
+
+
+/////////////////////////
+// Quering and Filtering
+/////////////////////////
+
+function FILTER(name, opsymbol, value) {
+  return [name, opsymbol, value];
+}
+
+function ORDER(name) {
+  return name;
+}
+
+function AND() {
+  var f = ['AND'];
+  for (var i = 0; i < arguments.length; i++) {
+    f.push(arguments[i]);
+  }
+  return f;
+}
+
+function OR() {
+  var f = ['OR'];
+  for (var i = 0; i < arguments.length; i++) {
+    f.push(arguments[i]);
+  }
+  return f;
+}
+
+var ModelFactory = function(type, opt_schema) {
+  var ignored_prefixes = ['_', '$'];
+
+  /**
+  * Query is an iterable collection of a Model
+  */
+  var Query = function() {
+    this._filter = [];
+    this._order = [];
+    this.projection = [];
+    this._page_size = 100;
+    this._more = false;
+    this._dirty = false;
+  };
+
+  Query.prototype = new Array();
+
+  /**
+  * onChange callback function
+  */
+  Query.prototype.onchange = undefined;
+
+  Query.prototype.next = function() {
+    return true;
+  };
+
+  Query.prototype.previous = function() {
+    return true;
+  };
+
+  Query.prototype.__defineGetter__('more', function() { return this._more; });
+
+  Query.prototype.__defineGetter__('page_size',
+      function() { return this._page_size; });
+  Query.prototype.__defineSetter__('page_size',
+      function(page_size) {
+        this._page_size = page_size;
+        this._dirty = true;
+      });
+
+  Query.prototype.filter = function() {
+    var filter;
+    switch (arguments.length) {
+      case 1:
+        filter = arguments[0];
+        break;
+      case 2:
+        filter = FILTER.apply(this,
+            arguments[0].split(' ').concat(arguments[1]));
+        break;
+      case 3:
+        filter = FILTER.apply(this, arguments);
+        break;
+      default:
+        throw Error('Undefined FILTER format.');
+    }
+    if (this._filter.length == 0) {
+      this._filter = ['AND'];
+    }
+    this._filter.push(filter);
+    return this;
+  };
+
+  Query.prototype.order = function(name) {
+    this._order.push(ORDER(name));
+    return this;
+  };
+
+  Query.prototype.serialize = function() {
+    return JSON.stringify({
+      filter: this._filter,
+      order: this._order,
+      projection: this.projection,
+      page_size: this._page_size
+    });
+  };
+
+  Query.prototype.fetch = function(opt_callback) {
+    var _this = this;
+    function callback(data) {
+      _this.length = 0;
+      _this.push.apply(_this, data);
+      var fn = opt_callback || _this.onchange;
+      if (fn) {
+        fn(_this);
+      }
+    }
+    http.GET('/api/' + type + '/?params=' + this.serialize(), callback);
+  };
+
+  function update(model, data) {
+    for (var k in data) {
+      model[k] = data[k];
+    }
+  }
+
+  var Model = function() {
+  };
+
+  /*
+   * Get a model by its id.
+   */
+  Model.get = function(id, opt_callback, opt_error) {
+    var m = new Model();
+    m.Id = id;
+    m.$update(opt_callback, opt_error);
+    return m;
+  };
+
+  /*
+   * query generates a iterator for a query object.
+   */
+  Model.query = function(opt_callback) {
+    var query = new Query();
+    // xhr query for collection with timeout to allow for chaining
+    setTimeout(function() {
+      query.fetch(opt_callback);
+    }, 0);
+    // tailbone.bind(type, function() { query.fetch() });
+    return query;
+  };
+
+  Model.serialize = function(model) {
+    var obj = {};
+    for (var member in model) {
+      if (ignored_prefixes.indexOf(member[0]) < 0) {
+        obj[member] = model[member];
+      }
+    }
+    return obj;
+  };
+
+  /*
+  * Dollar sign prefix names are ignored on the model as are underscore
+  * _name
+  * and
+  * $name
+  * are not included in the jsonifying of an object
+  */
+  Model.prototype.$save = function(opt_callback, opt_error) {
+    var _this = this;
+    http.POST('/api/' + type + '/', Model.serialize(this), function(data) {
+      update(_this, data);
+      var fn = opt_callback || _this.onchange;
+      if (fn) {
+        fn(_this);
+      }
+      // tailbone.trigger(type);
+    }, opt_error);
+  };
+
+  Model.prototype.$delete = function(opt_callback, opt_error) {
+    var _this = this;
+    http.DELETE('/api/' + type + '/' + this.Id, function() {
+      var fn = opt_callback || _this.onchange;
+      if (fn) {
+        fn();
+      }
+      // tailbone.trigger(type);
+    }, opt_error);
+  };
+
+  Model.prototype.$update = function(opt_callback, opt_error) {
+    var _this = this;
+    http.GET('/api/' + type + '/' + this.Id, function(data) {
+      update(_this, data);
+      var fn = opt_callback || _this.onchange;
+      if (fn) {
+        fn(_this);
+      }
+    }, opt_error);
+  };
+
+  return Model;
+};
+
+var User = new ModelFactory('users');
+
+function authorizeCallback(opt_callback) {
+
+  function processToken(callback) {
+    return function(message) {
+      if (message.data.type != 'Login') {
+        return;
+      }
+      var localhost = false;
+      if (message.origin.substr(0, 17) == 'http://localhost:') {
+        localhost = true;
+      }
+      if (!localhost && message.origin !== window.location.origin) {
+        throw new Error('Origin does not match.');
+      } else {
+        removeEventListener('message', process, false);
+        if (callback) {
+          callback(message.data.payload);
+        }
+      }
+    };
+  }
+
+  var process = processToken(opt_callback);
+  addEventListener('message', process, false);
+
+}
+
+// have a constructor for login url
+User.login_url = function(redirect_url) {
+  return '/api/login?url=' + (redirect_url || '/');
+};
+
+User.login_popup_url = function(opt_callback) {
+  authorizeCallback(opt_callback);
+  return User.login_url('/api/login.html');
+};
+
+User.login = function(opt_callback) {
+  var x, y;
+  if (window.screen.width) {
+    x = window.screenX + window.screen.width / 2.0;
+    y = window.screenY + window.screen.height / 2.0;
+  }
+
+  var pos = {
+    x: x,
+    y: y,
+    width: 1100,
+    height: 600
+  };
+
+  var prop = 'menubar=0, resizable=0, location=0, toolbar=0, ' +
+    'status=0, scrollbars=1, titlebar=0, left=' +
+    (pos.x - (pos.width / 2.0)) + ', top=' +
+    (pos.y - (pos.height / 2.0)) + ', width=' +
+    pos.width + ', height=' + pos.height;
+
+  window.open(User.login_popup_url(opt_callback), 'Auth', prop);
+};
+
+User.logout = function(opt_callback) {
+  http.GET('/api/logout?url=/api/users/me', null, opt_callback);
+};
+
+//
+// User.get('me', function(me) {
+//  if(!me) {
+//    User.login(function(me) {
+//      doSomething(me);
+//    });
+//  } else {
+//    doSomething(me);
+//  }
+// });
+
+// Add the channel js for appengine and bind the events.
+
+// Exports
+return {
+  Model: ModelFactory,
+  User: User,
+  FILTER: FILTER,
+  ORDER: ORDER,
+  AND: AND,
+  OR: OR,
+  trigger: function(name, payload) { ifConnected(trigger, name, payload); },
+  bind: function(name, fn) { ifConnected(bind, name, fn); },
+  unbind: function(name, fn) { ifConnected(unbind, name, fn); }
+};
+
+})(this, this.document);
