b7cfb3a86b4a95f475666c21fc7528bf996539a4
Doug Fritz <dougfritz@gmail.com>
2013-03-11 15:56:25 -0700

    html5mode to rewrite paths to index.html

--- tailbone/html5mode/__init__.py
+++ tailbone/html5mode/__init__.py
@@ -1,2 +1,23 @@
-# TODO: write a module to rewrite any path to the index.html file for
-# html5mode history and location.
+# Rewrites any path to the index.html file for html5mode history and location.
+
+# shared resources and global variables
+from tailbone import *
+
+import webapp2
+
+# index.html is symlinked to api/client/index.html
+index = None
+with open('tailbone/html5mode/index.html') as f:
+  index = f.read()
+
+# Html5mode Handler
+# ------------
+#
+# Proxies any page to the base url
+class Html5modeHandler(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write(index)
+
+app = webapp2.WSGIApplication([
+  (r"^[^.]*$", Html5modeHandler),
+  ], debug=DEBUG)
