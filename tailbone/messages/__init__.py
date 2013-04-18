7285a5bf7cfb0e9cfc72d5a434452d4b7af7f255
Doug Fritz <dougfritz@google.com>
2013-04-17 17:23:47 -0700

    simple messages api (more direct than events and no db storage)

---
+++ tailbone/messages/__init__.py
@@ -0,0 +1,48 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tailbone import *
+
+import json
+import random
+import string
+import webapp2
+
+from google.appengine.api import channel
+
+
+class MessagesHandler(BaseHandler):
+  @as_json
+  def get(self):
+    # randomly generate client_id
+    # collision probability with 50000 concurrent users and 8 digit client_id = 0.00000572499
+    # 1 - e^(-(50000^2)/(2*62^8))
+    client_id = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(8))
+    return {
+      "token": channel.create_channel(client_id),
+      "client_id": client_id
+    }
+
+  @as_json
+  def post(self):
+    body = self.request.body
+    data = json.loads(body)
+    to = data.get("to")
+    if not to:
+      raise AppError("Must provide a 'to' client_id.")
+    channel.send_message(to, body)
+
+app = webapp2.WSGIApplication([
+  (r"{}messages/.*".format(PREFIX), MessagesHandler),
+], debug=DEBUG)
