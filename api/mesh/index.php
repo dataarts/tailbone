b626b00b26e8e8c4760078933db2cf9ae67c30e4
Doug Fritz <dougfritz@gmail.com>
2013-06-04 11:48:09 -0400

    archive mesh in mesh branch

--- api/mesh/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// this file is temporary, will move to actual API later
-echo '{"url": "ws://' . $_SERVER['REMOTE_ADDR'] . ':12347/roomname", "name": "roomname", "cliend_id": "abcd", "password": "imsecret"}';
-
-?>
--- python/WebSocket/readme.txt
+++
@@ -1,7 +0,0 @@
-This is a WebSocket server for testing purposes.
-
-* INSTRUCTIONS *
-run: python server.py
-
-Following handlers are available:
-# TODO: add handlers list
