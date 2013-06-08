8e7a03427c530ba8cfc827866dc4cba8a706b367
Doug Fritz <dougfritz@gmail.com>
2013-06-08 13:26:57 -0700

    updated comments to docstring

--- tailbone/compute_engine/mesh/websocket.py
+++ tailbone/compute_engine/mesh/websocket.py
@@ -3,7 +3,6 @@
 # @author Maciej Zasada maciej@unit9.com
 ##
 
-import re
 import json
 import time
 from optparse import OptionParser
@@ -19,11 +18,9 @@ nodes_by_id = {}
 meshes_by_id = {}
 mesh_id_by_node = {}
 
-##
-# Joins a node to a mesh.
-# Creates new mesh if needed.
-##
+
 def enter(node, mesh_id):
+  """Joins a node to a mesh. Creates new mesh if needed."""
   node.id = new_node_id()
   nodes.append(node)
   nodes_by_id[node.id] = node
@@ -39,10 +36,9 @@ def enter(node, mesh_id):
   send_to_node(node, node, ['exist'] + get_exist(mesh, node.id))
   return True
 
-##
-# Removes node from meshes, disconnects node.
-##
+
 def leave(node):
+  """Removes node from meshes, disconnects node."""
   if not node:
     return
   mesh_id = mesh_id_by_node[node]
@@ -61,21 +57,17 @@ def leave(node):
     pass
   logging.debug('leave (node ID: %s, mesh ID: %s)' % (node.id, mesh_id))
 
-##
-# Simple time sync call
-##
+
 def time_sync(node, message):
+  """Simple time sync call."""
   if message[0] == 't':
-    t = message[1:]
-    now = time.time()*1000;
     node.write_message('t{:f}'.format(time.time()*1000))
     return True
   return False
 
-##
-# Interprets node message and directs it forward.
-##
+
 def parse_message(node, message):
+  """Interprets node message and directs it forward."""
   # quick check to see if this is a time sync call
   if time_sync(node, message):
     return
@@ -92,20 +84,18 @@ def parse_message(node, message):
     return
   send_to_node_ids(to_nodes, node, message_data)
 
-##
-# Wraps message with sender ID and timestamp
-##
+
 def wrap_message(message, sender_node):
+  """Wraps message with sender ID and timestamp."""
   try:
     message_string = json.dumps([sender_node.id, time.time(), message])
     return message_string
   except:
     return None
 
-##
-# Sends message to a node.
-##
+
 def send_to_node(node, sender_node, message):
+  """Sends message to a node."""
   message_string = wrap_message(message, sender_node)
   if message_string:
     logging.info('sending to node %s (node ID: %s, to ID: %s)' % (message_string, sender_node.id, node.id))
@@ -114,18 +104,16 @@ def send_to_node(node, sender_node, message):
     except:
       pass
 
-##
-# Sends message to array of nodes
-##
+
 def send_to_node_ids(node_ids, sender_node, message):
+  """Sends message to array of nodes."""
   for node_id in node_ids:
     if node_id in nodes_by_id:
       send_to_node(nodes_by_id[node_id], sender_node, message)
 
-##
-# Sends message to a mesh
-##
+
 def send_to_mesh(mesh, sender_node, message):
+  """Sends message to a mesh."""
   message_string = wrap_message(message, sender_node)
   if message_string:
     logging.info('sending to mesh %s (node ID: %s, mesh ID: *)' % (message_string, sender_node.id))
@@ -138,25 +126,22 @@ def send_to_mesh(mesh, sender_node, message):
   else:
     logging.warning('invalid message format %s' % message)
 
-##
-# Gets a list of connected node IDs by mesh
-##
+
 def get_exist(mesh, ignore):
+  """Gets a list of connected node IDs by mesh."""
   return [node.id for node in mesh if node.id != ignore]
 
-##
-# Generates new node ID.
-##
+
 def new_node_id():
+  """Generates new node ID."""
   global node_id_seed
   node_id = node_id_seed
   node_id_seed = node_id_seed + 1
   return node_id
 
-##
-# WebSocket connection and message handler
-##
+
 class Handler(tornado.websocket.WebSocketHandler):
+  """WebSocket connection and message handler."""
   def open(self):
     enter(self, self.request.path[1:])
 
@@ -166,11 +151,9 @@ class Handler(tornado.websocket.WebSocketHandler):
   def on_close(self):
     leave(self)
 
-##
-# Instantiates WebSocket server
-# Usage: TODO: add usage notes
-##
+
 def main():
+  """Instantiates WebSocket server Usage: TODO: add usage notes"""
   parser = OptionParser()
   parser.add_option('-d', '--debug', dest='debug', action='store_true', help='enables debug mode', metavar='DEBUG', default=False)
   parser.add_option('-p', '--port', dest='port', help='port number to run the server on', metavar='PORT', default=2345)
