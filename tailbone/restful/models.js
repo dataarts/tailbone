15b6bea3a0fe049250ae94c57d1c909364cf691f
Aleksandar Rodic <aleksandar.xyz@gmail.com>
2014-01-28 18:43:29 -0800

    add Emulate legacy getter/setter API using ES5 APIs.

--- tailbone/restful/models.js
+++ tailbone/restful/models.js
@@ -9,6 +9,32 @@
 // These are some useful helper functions for constructing queries, although you
 // probably won't have to use these directly, see the query section below.
 
+
+// Emulate legacy getter/setter API using ES5 APIs.
+// Since __defineGetter__ and __defineSetter__ are not supported any longer by IE9 or 10 or Windows 8, and Box2D for javascript v2.1a still uses them, this shim is required to run Box2D in those environments.
+// This is taken directly from Allen Wirfs-Brock's blog at: 
+// http://blogs.msdn.com/b/ie/archive/2010/09/07/transitioning-existing-code-to-the-es5-getter-setter-apis.aspx
+try {
+  if(!Object.prototype.__defineGetter__ &&
+  Object.defineProperty({}, "x", { get: function() { return true } }).x) {
+    Object.defineProperty(Object.prototype, "__defineGetter__",
+   { enumerable: false, configurable: true,
+    value: function(name, func) {
+      Object.defineProperty(this, name,
+       { get: func, enumerable: true, configurable: true });
+    } 
+   });
+    Object.defineProperty(Object.prototype, "__defineSetter__",
+   { enumerable: false, configurable: true,
+    value: function(name, func) {
+      Object.defineProperty(this, name,
+       { set: func, enumerable: true, configurable: true });
+    } 
+   });
+  }
+} catch(defPropException) { /*Do nothing if an exception occurs*/ };
+////////////////////////
+
 function FILTER(name, opsymbol, value) {
   return [name, opsymbol, value];
 }
